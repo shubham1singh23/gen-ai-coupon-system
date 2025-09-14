5c7a9530bc28d13262b41dd8cd0f856637324d42
SHUBHAM <shubham1singh23@gmail.com>
2025-09-14 18:34:02 +0530

    new app password

--- client/src/config/axios.js
+++ client/src/config/axios.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 // Create axios instance with base URL
 const api = axios.create({
-    baseURL: 'http://localhost:5000',
+    baseURL: 'https://gen-ai-coupon-system.onrender.com',
     withCredentials: true,
     headers: {
         'Content-Type': 'application/json',
